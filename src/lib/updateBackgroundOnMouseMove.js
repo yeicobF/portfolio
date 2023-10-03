23f8f8d5ce82e0bb5abb6e2fb1067e806af71094
yeicob <jacobrdz34@gmail.com>
2023-10-03 02:43:52 -0600

    feat: add function to update background gradient on mouse move

---
+++ src/lib/updateBackgroundOnMouseMove.js
@@ -0,0 +1,15 @@
+const updateBackgroundOnMouseMove = (event) => {
+  // Selecciona el elemento en el que deseas escuchar los eventos de ratón.
+  const body = document.body
+
+  // Escucha el evento "mousemove" en el elemento seleccionado.
+  // Obtiene las coordenadas del cursor en relación con el elemento.
+  const x = (event.clientX / body.clientWidth) * 100
+  const y = (event.clientY / body.clientHeight) * 100
+
+  // Actualiza el fondo con las coordenadas calculadas.
+  body.style.background = `radial-gradient(600px at ${x}% ${y}%, rgba(192, 13, 224, 0.15), transparent 80%)`
+}
+
+// Escucha el evento "mousemove" en el elemento seleccionado.
+document.body.addEventListener("mousemove", updateBackgroundOnMouseMove)
