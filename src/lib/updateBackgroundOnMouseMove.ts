9add7bc08df22a42b0e895c87b3941de7245c14f
yeicob <jacobrdz34@gmail.com>
2023-10-03 06:49:55 -0600

    feat: persist one background gradient and track the mouse with another

--- src/lib/updateBackgroundOnMouseMove.ts
+++ src/lib/updateBackgroundOnMouseMove.ts
@@ -9,8 +9,12 @@ const updateBackgroundOnMouseMove = (event: MouseEvent): void => {
   const x = (event.clientX / body.clientWidth) * 100
   const y = (event.clientY / body.clientHeight) * 100
 
+  const ORIGINAL_GRADIENT =
+    "radial-gradient(800px at 10% 15%, rgba(192, 13, 224, 0.30), transparent 80%)"
+  const trackingGradient = `radial-gradient(600px at ${x}% ${y}%, rgba(192, 13, 224, 0.15), transparent 90%)`
+
   // Actualiza el fondo con las coordenadas calculadas.
-  body.style.background = `radial-gradient(600px at ${x}% ${y}%, rgba(192, 13, 224, 0.15), transparent 80%)`
+  body.style.background = `${ORIGINAL_GRADIENT}, ${trackingGradient}`
 }
 
 // Escucha el evento "mousemove" en el elemento seleccionado.
