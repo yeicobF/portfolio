ae61c614c3f8de438ab44dbdb23d260a02e73e89
yeicob <jacobrdz34@gmail.com>
2023-10-03 03:49:20 -0600

    feat: add vercel web vitals and speed insights to config
    The docs implied that 'output' must be 'server', but I want the app
    to be static rendered, so I specified
    'static'.
    
    I do not know if this will cause any trouble. If it does, I'll change it.
    
    The docs say: Available for: Serverless, Edge, Static
    
    https://docs.astro.build/en/guides/integrations-guide/vercel/#web-analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/serverless"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://www.jacobflores.dev"
-});
+  site: "https://www.jacobflores.dev",
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    speedInsights: {
+      enabled: true,
+    },
+  }),
+})
