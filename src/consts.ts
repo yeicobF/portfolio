7c4fc998e1ba193b197a5f0f65c4b163d7c19266
yeicob <jacobrdz34@gmail.com>
2023-10-30 01:22:24 -0600

    build: export constants

---
+++ src/consts.ts
@@ -0,0 +1,15 @@
+export const ME = {
+  name: "Jacob Flores",
+  role: "Frontend Developer",
+  company: {
+    name: "",
+    web: "",
+  },
+}
+
+export const GRADIENTS = {
+  original:
+    "linear-gradient(180deg, rgba(0, 0, 0, 0.38) 0%, rgba(11, 7, 24, 0.77) 45.83%, rgba(11, 7, 25, 0.82) 58.85%, #0b0b0b 80.73%)",
+  reversed:
+    "linear-gradient(180deg, #0b0b0b 0%, rgba(11, 7, 25, 0.82) 19.27%, rgba(11, 7, 24, 0.77) 41.15%, #0b0b0b 100%)",
+}
