1efc5013497658915a3e741ffaabff8f82b75007
Jacob Flores <jflores@qualtrics.com>
2024-12-07 00:02:38 -0600

    fix: update vercel and remove deprecated segment
    The "@astrojs/vercel/static" import is deprecated and will be removed in a future release. Please import from "@astrojs/vercel" instead.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
 import tailwind from "@astrojs/tailwind"
 // https://docs.astro.build/en/guides/integrations-guide/vercel/#targets
-import vercel from "@astrojs/vercel/static"
+import vercel from "@astrojs/vercel"
 
 // https://astro.build/config
 export default defineConfig({
